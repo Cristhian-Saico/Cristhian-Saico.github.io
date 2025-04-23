3c84b2e10a966cf88421a3699c704088291a2c6c
Cristhian-Saico <cristhiandanielml@gmail.com>
2025-04-23 00:42:57 -0500

    repositorio-actualizacion

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-document.getElementById('loadingHeartContainer').addEventListener('click', function() {
-    const fillElement = document.querySelector('.fill');
-    fillElement.style.animation = 'fillHeart 2s linear forwards';
-    
-    setTimeout(function() {
-        fillElement.addEventListener('animationend', function() {
-            window.location.href = "aniversario.html"; // Cambia a la página de destino
-        });
-    }, 2000);
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Gracias por tu mensaje, ${name}! Nos pondremos en contacto contigo pronto.`);
 });
-
-function createHearts() {
-    const heartsContainer = document.getElementById('hearts');
-    for (let i = 0; i < 30; i++) {
-        let heart = document.createElement('div');
-        heart.className = 'heart';
-        heart.style.left = Math.random() * 100 + 'vw';
-        heart.style.top = Math.random() * 100 + 'vh';
-        heart.style.animationDuration = Math.random() * 2 + 3 + 's';
-        heartsContainer.appendChild(heart);
-    }
-}
-
-// Llama a createHearts() si es necesario en la página de aniversario
-if (document.getElementById('hearts')) {
-    createHearts();
-}
